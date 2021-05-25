3cebe186892867ff71ccbbf4a7b02f1bc96f5c3f
xSuper <81652889+xSuperr@users.noreply.github.com>
2021-05-25 18:39:54 -0400

    Update encode.go

--- minecraft/nbt/encode.go
+++ minecraft/nbt/encode.go
@@ -2,13 +2,11 @@ package nbt
 
 import (
 	"bytes"
-	"errors"
 	"io"
 	"math"
 	"reflect"
 	"strings"
 	"sync"
-	"unicode/utf8"
 )
 
 // Encoder writes NBT objects to an NBT output stream.
@@ -196,9 +194,6 @@ func (e *Encoder) encode(val reflect.Value, tagName string) error {
 
 	case reflect.String:
 		s := val.String()
-		if !utf8.ValidString(s) {
-			return InvalidStringError{Off: e.w.off, String: s, Err: errors.New("string does not exist out of utf8 only")}
-		}
 		return e.Encoding.WriteString(e.w, s)
 
 	case reflect.Slice:
